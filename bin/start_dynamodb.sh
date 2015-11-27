a44ce92c851b11302857e5564456ac844f31f360
Tom Parker <palfrey@tevp.net>
2015-11-27 14:30:45 +0000

    Make start_dynamodb.sh fail if any of the commands fail

--- bin/start_dynamodb.sh
+++ bin/start_dynamodb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail ; IFS=$'\t\n'
+
 pkill -9 -f DynamoDBLocal || true
 screen -dm -S dynamodb_local java -Djava.library.path=./dynamodb_local/DynamoDBLocal_lib -jar dynamodb_local/DynamoDBLocal.jar -inMemory
 sleep 1
