ba233fec1cee0844f6dcae0874f9368cf9ed7974
Tom Parker <palfrey@tevp.net>
2015-11-27 14:54:22 +0000

    Stop DynamoDB from locking up EUnit

--- bin/start_dynamodb.sh
+++ bin/start_dynamodb.sh
@@ -4,7 +4,7 @@ set -euo pipefail ; IFS=$'\t\n'
 if [ -f dynamodb.pid ]; then
 	kill $(cat dynamodb.pid) || true
 fi
-java -Djava.library.path=./dynamodb_local/DynamoDBLocal_lib -jar dynamodb_local/DynamoDBLocal.jar -inMemory &
+nohup java -Djava.library.path=./dynamodb_local/DynamoDBLocal_lib -jar dynamodb_local/DynamoDBLocal.jar -inMemory > dynamodb.out 2> dynamodb.err < /dev/null &
 echo $! > dynamodb.pid
 sleep 1
 echo '==> local dynamo (started)'
