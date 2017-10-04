26cc537dd2ed110d1f0038c0a1aa7dd80de2c263
Sam Tavakoli <sam@gameanalytics.com>
2017-10-04 12:48:08 +0100

    Making Rebar3 and Travis play ball
    * Switching from SSH to HTTPS for deps to make travis-ci.org happy.
    * Using eproxus repo since it's a test dependency
    * Merging setup/start scripts for ddb local since Rebar3 doesn't have get-deps hook.
    * Defining what xref checks to perform
    * The previous sleep 1 isn't enough for travis. Trying to check if end point is up before continuing, awful with jars!

--- bin/setup_dynamodb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo '==> local dynamo (pre_hook)'
-mkdir -p dynamodb_local
-wget -nc http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.zip -O dynamodb_local/dynamodb_local_latest.zip
-unzip -n dynamodb_local/dynamodb_local_latest.zip -d dynamodb_local
-echo '==> local dynamo (pre_hook) [done]'
-exit 0
