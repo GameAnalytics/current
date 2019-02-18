489ebf65bbf083e83c4440fa8ed58ede1b4c21e0
Tomasz Wójcikowski <tomaszwojcikowski@users.noreply.github.com>
2019-02-18 10:26:06 +0100

    Send case sensitive headers to AWS  (#25)
    * Send {headers_as_is, true} with httpc
    
    * Add content-length

--- src/current_http_client.erl
+++ src/current_http_client.erl
@@ -22,8 +22,9 @@
                   response_ok() | response_error().
 post(URL, Headers, Body, Opts) ->
     CallTimeout   = proplists:get_value(call_timeout,    Opts, 10000),
-    Request = {to_list(URL), normalize_headers(Headers), "", Body},
-    Options = [{body_format, binary}],
+    Headers2 = [{"Content-Length", integer_to_list(erlang:size(Body))} | Headers],
+    Request = {to_list(URL), normalize_headers(Headers2), "", Body},
+    Options = [{body_format, binary}, {headers_as_is, true}],
     case httpc:request(post, Request, [{timeout, CallTimeout}], Options) of
         {ok,{{_,Code,_},_Headers, RetBody}} ->
             {ok, Code, RetBody};
